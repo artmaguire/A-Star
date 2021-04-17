b1833d5a184c1ff1cb4729aacab47807f2bab750
artmaguire <a.elnino11@gmail.com>
2021-04-17 18:58:14 +0100

    Walk distance params changes

--- dfosm/weighting/WalkWeighter.py
+++ dfosm/weighting/WalkWeighter.py
@@ -17,7 +17,7 @@ class WalkWeighter(Weighter):
             if node.clazz < 30 or node.clazz == 43:
                 cost /= 3
             else:
-                cost *= 3
+                cost *= 5
         # End
         elif node.distance < 8:
             if node.clazz < 30 or node.clazz == 43:
@@ -28,8 +28,10 @@ class WalkWeighter(Weighter):
         elif (node.distance / node.node_options.starting_distance) > .5:
             if node.clazz < 30 or node.clazz == 43:
                 if node.kmh >= 100:
-                    cost *= 0.2
+                    cost *= 0.1
                 elif node.kmh >= 80:
+                    cost *= 0.2
+                elif node.kmh >= 50:
                     cost *= 0.3
                 else:
                     cost *= 1
@@ -74,27 +76,27 @@ class WalkWeighter(Weighter):
             if node.clazz < 30 or node.clazz == 43:
                 delta /= 3
             else:
-                delta *= 3
+                delta *= 6
         # Middle End
         elif (node.distance / node.node_options.starting_distance) > .5:
             if node.clazz < 30 or node.clazz == 43:
                 if node.kmh >= 100:
-                    delta *= 0.1
+                    delta *= 0.05
                 elif node.kmh >= 80:
-                    delta *= 0.15
+                    delta *= 0.08
                 elif node.kmh >= 50:
-                    delta *= 0.2
+                    delta *= 0.13
                 else:
                     delta *= 1
         # Middle Start
         else:
             if node.clazz <= 22 or node.clazz == 43:
                 if node.kmh >= 100:
-                    delta *= 0.1
+                    delta *= 0.05
                 elif node.kmh >= 80:
-                    delta *= 0.15
+                    delta *= 0.08
                 elif node.kmh >= 50:
-                    delta *= 0.2
+                    delta *= 0.13
                 else:
                     delta *= 100
             else:
