af3869a470ae33ba8496f317660ba5f848dca3c9
artmaguire <a.elnino11@gmail.com>
2021-04-18 18:09:55 +0100

    Bike weights adjusted

--- dfosm/weighting/BikeWeighter.py
+++ dfosm/weighting/BikeWeighter.py
@@ -13,43 +13,27 @@ class BikeWeighter(Weighter):
             return cost / 10
 
         # Start
-        if node.cost_minutes < 8:
+        if node.cost_minutes < 3:
             if node.clazz < 30 or node.clazz == 43:
-                cost /= 3
+                cost /= 1
             else:
-                cost *= 3
+                cost *= 1
         # End
         elif node.distance < 8:
             if node.clazz < 30 or node.clazz == 43:
-                cost /= 3
+                cost /= 0.7
             else:
-                cost *= 3
+                cost *= 1
         # Middle End
         elif (node.distance / node.node_options.starting_distance) > .5:
             if node.clazz < 30 or node.clazz == 43:
-                if node.kmh >= 100:
-                    cost *= 0.2
-                elif node.kmh >= 80:
-                    cost *= 0.3
-                else:
-                    cost *= 1
+                cost *= 1
         # Middle Start
         else:
             if node.clazz <= 22 or node.clazz == 43:
-                if node.kmh >= 100:
-                    cost *= 0.1
-                elif node.kmh >= 80:
-                    cost *= 0.2
-                elif node.kmh >= 50:
-                    cost *= 0.3
-                else:
-                    cost *= 100
+                cost *= 0.7
             else:
-                cost *= 5
-
-        # if cost == 0.014238:
-        #     print(node.node_id)
-        #     pass
+                cost *= 1
 
         return cost
 
@@ -66,38 +50,24 @@ class BikeWeighter(Weighter):
             return 0
 
         # Start
-        # if node.cost_minutes < 8:
-        #     if node.clazz < 30 or node.clazz == 43:
-        #         delta /= 3
+        if node.cost_minutes < 3:
+            if node.clazz < 30 or node.clazz == 43:
+                delta /= 1
         # End
         elif node.distance < 8:
             if node.clazz < 30 or node.clazz == 43:
-                delta /= 3
+                delta *= 2
             else:
-                delta *= 3
+                delta *= 2
         # Middle End
         elif (node.distance / node.node_options.starting_distance) > .5:
             if node.clazz < 30 or node.clazz == 43:
-                if node.kmh >= 100:
-                    delta *= 0.1
-                elif node.kmh >= 80:
-                    delta *= 0.15
-                elif node.kmh >= 50:
-                    delta *= 0.2
-                else:
-                    delta *= 1
+                delta *= 1.2
         # Middle Start
         else:
             if node.clazz <= 22 or node.clazz == 43:
-                if node.kmh >= 100:
-                    delta *= 0.1
-                elif node.kmh >= 80:
-                    delta *= 0.15
-                elif node.kmh >= 50:
-                    delta *= 0.2
-                else:
-                    delta *= 100
+                delta *= 1.2
             else:
-                delta *= 5
+                delta *= 1
 
         return delta
